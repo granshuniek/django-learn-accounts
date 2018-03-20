93c3bc1d38cf2126ea5a47041a59cf4875186a8b
granshuniek <granshuniek@gmail.com>
2018-03-20 19:39:10 +0100

    End part 3 of tutorial

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
 class Genre(models.Model):
     name = models.CharField(max_length=200, help_text="Enter a book genre.")
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    language = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -12,6 +19,7 @@ class Book(models.Model):
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text="Enter a short book description")
     isbn = models.CharField('ISBN', max_length=13)
+    original_language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
     def __str__(self):
@@ -49,7 +57,7 @@ class BookInstance(models.Model):
         return "{0} ({1})".format(self.id, self.book.title)
 
 class Author(models.Model):
-    firs_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
