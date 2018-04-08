b97bcedad89fa4535bd0dd349f1b383ee9767169
gransh <granshuniek@gmail.com>
2018-04-08 02:48:40 +0200

    Start to learn user authentication

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,6 +9,9 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     num_genres = Genre.objects.count()
     serached_word = r'Imię'
     titles_with_particular_word = Book.objects.filter(title__regex=serached_word).count()
@@ -22,7 +25,8 @@ def index(request):
             'num_authors': num_authors,
             'num_genres': num_genres,
             'searched_word': serached_word,
-            'titles_with_word': titles_with_particular_word
+            'titles_with_word': titles_with_particular_word,
+            'num_visits': num_visits
         },
     )
 
